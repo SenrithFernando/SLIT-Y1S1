b8db44a8738b57fa544636ea4c93fe0c4ece2f01
SFernando Solutions <143948014+SFernandoSolutions@users.noreply.github.com>
2023-10-04 15:29:28 +0530

    OnlineExam_02_Q1.c

---
+++ OnlineExam_02_Q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void){
+	int typeOfCustomer, billAmount, unit, units;
+	
+	printf("Enter the type of customer (1-Domestic customer / 2-samurdhi customer) : ");
+	scanf("%d",&typeOfCustomer);
+	
+	printf("Enter the Units : ");
+	scanf("%d",&unit);
+	
+	if(typeOfCustomer==1){
+		if(unit>100){
+			billAmount=(100*10)+((unit-100)*30)+((unit-100)*(20/100));
+		}else{
+			billAmount=(unit*10);
+		}
+	}else if(typeOfCustomer==2){
+		if(unit>100){
+			billAmount=(100*10)+((unit-100)*30);
+		}else{
+			billAmount=(unit*10);
+		}
+	}else{
+		printf("Invalid Customer ");
+	}
+	
+	printf("Your Bill amount is : %d",billAmount);
+}
