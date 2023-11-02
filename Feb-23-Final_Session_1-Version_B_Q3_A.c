1717ba9d9753ad772b0c65e4648836a40968521a
Senrith Fernando <115388481+SenrithFernando@users.noreply.github.com>
2023-11-02 17:05:49 +0530

    Create Feb-23-Final_Session_1-Version_B_Q3_A.c

---
+++ Feb-23-Final_Session_1-Version_B_Q3_A.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char strArr[15];
+    int i, j;
+    char temp;
+
+    printf("Enter a text (up to 15 characters): ");
+    scanf("%15s", strArr); // Input text directly into the array
+
+    // Find the length of the input string
+    for (i = 0; strArr[i] != '\0' && i < 15; i++) {
+        // Do nothing, just find the length
+    }
+
+    // Reverse the elements of the array
+    for (j = 0; j < i / 2; j++) {
+        printf("%d\t%d\n",i,j);
+        temp = strArr[j];
+        strArr[j] = strArr[i - j - 1];
+        strArr[i - j - 1] = temp;
+    }
+
+    printf("Reversed text: %s\n", strArr);
+
+    return 0;
+}
